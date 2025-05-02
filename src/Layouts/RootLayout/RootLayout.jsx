cc576fdaf4116928cfedb11e28e744faa4110f8b
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-05-02 18:40:24 +0600

    added navbar section

--- src/Layouts/RootLayout/RootLayout.jsx
+++ src/Layouts/RootLayout/RootLayout.jsx
@@ -2,9 +2,19 @@ import React from 'react';
 
 const RootLayout = () => {
     return (
-        <div>
-            <h1 className='text-2xl text-red-500'>This is RootLayout</h1>
+        <div className='relative h-screen'>
+            {/* Blurred Background Image */}
+            <div className='absolute inset-0 bg-[url("https://i.ibb.co/21k561wd/Rectangle-1.png")] bg-cover bg-center filter  z-0'></div>
+
+            {/* Dark Overlay */}
+            <div className='absolute inset-0 bg-black/30 z-0'></div>
+
+            {/* Content */}
+            <div className='relative z-10 flex h-full'>
+                <h1 className='text-4xl text-white font-bold'>This is RootLayout</h1>
+            </div>
         </div>
+
     );
 };
 
